f3dfbe5f2b55af450fa09062f26b6689f0ed15b1
Mics21 <schneider.mic@gmx.de>
2016-11-16 16:36:33 +0100

    implemented A2-Save data as xml

--- htdocs/php/world_data_parser.php
+++ htdocs/php/world_data_parser.php
@@ -15,6 +15,27 @@ class WorldDataParser {
         }
     }
 
+    /**
+    * #TODO: add return value (boolean)
+    */
+
+    public function saveXML(array $data) {
+        $handle = fopen('../data/wold_data.xml', 'w');
+        $content = '<xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        $content .= '<Countries>'  .PHP_EOL;
+        for($i = 1; $i < count($data); $i++) {
+            $content .= '<Country>' . PHP_EOL;
+            for($j = 0; $j < count($data[0]); $j++) {            
+                $elemName = str_replace(' ', '_', trim($data[0][$j]));
+                $content .= '<' . $elemName . '>' . trim($data[$i][$j]) . '</' . $elemName . '>' . PHP_EOL;
+            }
+            $content .= '</Country>' . PHP_EOL;
+        }
+        $content .= '</Countries>';
+        fwrite($handle, $content);
+        fclose($handle);
+    }
+
 }
 
 
