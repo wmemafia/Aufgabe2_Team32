e8a8b9da6e0ea06ebe469032386bf6592c8d6dac
Chris Kepler <development@kepler.international>
2016-11-22 08:59:37 +0100

    Update parse.php

--- htdocs/php/parse.php
+++ htdocs/php/parse.php
@@ -70,7 +70,7 @@ $data = $parser->parseCSV('../data/world_data.csv');
 						Copyright &copy; 2015 world_data
 					</p>
 					<p>
-						First course exercise <strong>HTML, CSS and JS</strong> of the lecture Web and Multimedia Engineering.
+						Second course exercise <strong>HTML, CSS and JS</strong> of the lecture Web and Multimedia Engineering.
 					</p>
 				</div><!-- /footer left column -->
 				<div class="small-100 medium-50 text-right">
